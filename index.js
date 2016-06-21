e69010e2858e1a51473a1eda2d2fc1d600f3216b
Joe Rehfuss <joe.rehfuss@gmail.com>
2016-06-20 18:12:45 -0700

    index and show skeletons complete

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var db = require('./models/');
 var gardenController = require('./controllers/gardens.js')
@@ -7,6 +8,7 @@ var ejsLayouts = require('express-ejs-layouts');
 app.use(ejsLayouts);
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/', express.static(__dirname + '/static/'));
 
 app.get('/', function(req, res) {
