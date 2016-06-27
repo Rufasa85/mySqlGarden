18025b4a41ae7590d43784e36f78aa7f572088b0
Joe Rehfuss <joe.rehfuss@gmail.com>
2016-06-27 03:02:47 -0700

    rds

--- index.js
+++ index.js
@@ -6,13 +6,6 @@ var gardenController = require('./controllers/gardens.js');
 
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  host     : process.env.RDS_HOSTNAME,
-  user     : process.env.RDS_USERNAME,
-  password : process.env.RDS_PASSWORD,
-  port     : process.env.RDS_PORT
-});
-
 var ejsLayouts = require('express-ejs-layouts');
 app.use(ejsLayouts);
 app.set('view engine', 'ejs');
@@ -25,5 +18,16 @@ app.get('/', function(req, res) {
 });
 
 app.use('/garden', gardenController);
+
+var env = process.env.NODE_ENV || 'development';
+if ('production' == env) {
+   // configure stuff here
+  console.log('Using production settings.');
+  app.set('connection', mysql.createConnection({
+    host: process.env.RDS_HOSTNAME,
+    user: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    port: process.env.RDS_PORT}));
+};
 
 app.listen(process.env.PORT || 8080);
